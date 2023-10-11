06df4b6f262c01578b249b917c858e49e8fad2c3
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-10-11 14:58:26 +0200

    add management Panel

--- client/src/pages/admin/managementPanel/managementPanel.tsx
+++ client/src/pages/admin/managementPanel/managementPanel.tsx
@@ -1,9 +1,38 @@
-
+import ManagementWindow from "./components/menagmentWindow";
+import { Box } from "@mui/material";
 const ManagmentPanel = () => {
 
     return(
-        <>
-        </>
+        <Box
+            sx={{
+                width:'100%',
+                height:'100%',
+                boxSizing:'border-box',
+                padding:'1rem',
+                display:'flex',
+                flexWrap:'wrap',
+                justifyContent:'space-around',
+            }}
+        >
+            <ManagementWindow 
+                title="Zwierzęta"
+            />
+            <ManagementWindow 
+                title="Zwierzęta"
+            />
+            <ManagementWindow 
+                title="Zwierzęta"
+            />
+            <ManagementWindow 
+                title="Zwierzęta"
+            />
+            <ManagementWindow 
+                title="Zwierzęta"
+            />
+            <ManagementWindow 
+                title="Zwierzęta"
+            />
+        </Box>
     )
 }
 
