a6a184ed186c4fc72c4142267b09915046ec3663
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-10-11 15:00:04 +0200

    add new routes for managementr and animals

--- client/src/utils/routes/adminRoutes.tsx
+++ client/src/utils/routes/adminRoutes.tsx
@@ -4,6 +4,8 @@ import LoginPage from '../../pages/admin/loginPage';
 import Panel from '../../pages/admin/adminPanel/panel';
 import { Box } from "@mui/material"
 import AdminMenu from "../../components/adminMenu"
+import ManagmentPanel from '../../pages/admin/managementPanel/managementPanel';
+import PetsManagement from '../../pages/admin/managementPanel/pages/petsManagement';
 const AdminRoutes = () => {
   return (
     <>
@@ -17,6 +19,8 @@ const AdminRoutes = () => {
       <AdminMenu/>
     <Routes>
           <Route path="/panel" element={<Panel/>} />
+          <Route path="/management" element={<ManagmentPanel/>} />
+          <Route path='/management/animals' element={<PetsManagement/>} />  
     </Routes>
     </Box>
     </>
