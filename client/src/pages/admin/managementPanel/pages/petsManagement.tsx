00d7f502923e279b7519be18d88e6040d0ad7952
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-10-11 14:58:59 +0200

    Add pets Management

---
+++ client/src/pages/admin/managementPanel/pages/petsManagement.tsx
@@ -0,0 +1,27 @@
+import { Box, useTheme } from "@mui/material";
+import AddAnimalForm from "../components/addAnimalForm";
+import AnimalList from "../components/animalList";
+
+const PetsManagement = () => {
+    const theme = useTheme();
+    return(
+        <Box
+            sx={{
+                width:'100%',
+                height:'100%',
+                boxSizing:'border-box',
+                padding:'1rem',
+                display:'flex',
+                flexWrap:'wrap',
+                justifyContent:'space-around',
+            }}
+        >
+            <AddAnimalForm/>
+            <AnimalList/>
+            <AnimalList/>
+        
+        </Box>
+    )
+}
+
+export default PetsManagement;
