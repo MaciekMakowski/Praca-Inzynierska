6bcdb4a6765b74920ab3c9a1f35d721a2e0e1e46
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-10-22 18:01:11 +0200

    add prettier to userRoutes

--- client/src/utils/routes/userRoutes.tsx
+++ client/src/utils/routes/userRoutes.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from '../../pages/home';
-import Needs from '../../pages/ourNeeds';
-import Contact from '../../pages/contact';
-import Adoption from '../../pages/adoption';
-import Footer from '../../components/footer';
-import Navbar from '../../components/navbar';
-import { Box } from '@mui/material';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../../pages/home";
+import Needs from "../../pages/ourNeeds";
+import Contact from "../../pages/contact";
+import Adoption from "../../pages/adoption";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
+import { Box } from "@mui/material";
 const UserRoutes = () => {
   return (
     <>
-        <Navbar/>
-            <Box
-                mt={'5rem'}
-            >
-                <Routes>
-                    <Route path="/*" element={<Home />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/needs" element={<Needs />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/adoption" element={<Adoption />} />
-                </Routes>
-            </Box>
-        <Footer/>
+      <Navbar />
+      <Box mt={"5rem"}>
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/needs" element={<Needs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/adoption" element={<Adoption />} />
+        </Routes>
+      </Box>
+      <Footer />
     </>
   );
 };
