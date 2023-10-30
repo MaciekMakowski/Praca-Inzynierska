e59c9406dbcfb55bcc08372fdd3c6aa1ccf590e5
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-10-30 14:29:04 +0100

    add dynamic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,20 @@
-
-import {createTheme, ThemeProvider} from "@mui/material";
-import {themeOptions} from "./assets/theme";
-import { BrowserRouter as Router } from 'react-router-dom';
 import {Route, Routes, useLocation} from "react-router-dom";
-import UserRoutes from "./utils/routes/userRoutes";
+import {ThemeProvider, createTheme} from "@mui/material";
+
 import AdminRoutes from "./utils/routes/adminRoutes";
 import LoginRoutes from "./utils/routes/loginRoutes";
+import { BrowserRouter as Router } from 'react-router-dom';
+import UserRoutes from "./utils/routes/userRoutes";
+import {themeOptions} from "./assets/theme";
+
 function App() {
     const theme = createTheme(themeOptions)
-
+    const location = useLocation()
   return (
       <>
           <ThemeProvider theme={theme}>
               <Router>
-                <Routes>
+                <Routes location={location} key={location.pathname}>
                     <Route path="/*" element={<UserRoutes />} />
                     <Route path="/admin" element={<LoginRoutes />} />
                     <Route path="/admin/*" element={<AdminRoutes />} />
