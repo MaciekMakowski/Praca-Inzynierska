fd427e5796f891509724f7b3f3f300a09236b055
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-11-14 19:27:03 +0100

    add universal validator

---
+++ client/src/utils/functions/validators.ts
@@ -0,0 +1,31 @@
+import { setAsError, unsetAsError } from "./setters";
+
+import { ErrorInput } from "../types/errorInput";
+
+interface BaseAttributes {
+  [key: string]: string;
+}
+
+interface FormAttributes<T extends BaseAttributes> {
+  attributes: T;
+}
+
+export const validateForm = async <T extends BaseAttributes>(
+  formData: FormAttributes<T>["attributes"],
+  foo: React.Dispatch<React.SetStateAction<ErrorInput>>
+) => {
+  for (const key in formData) {
+    if (formData.hasOwnProperty(key)) {
+      const value = formData[key];
+
+      if (value === "") {
+        setAsError(key, foo);
+        return false;
+      } else {
+        unsetAsError(key, foo);
+      }
+    }
+  }
+
+  return true;
+};
