1fb3ceca36256c6f4a7545fe682629838a51120e
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-11-30 14:43:02 +0100

    fix forwarding  to adoptionDetails

--- client/src/components/managementPanel/lists/adoptionListItem.tsx
+++ client/src/components/managementPanel/lists/adoptionListItem.tsx
@@ -104,7 +104,7 @@ const AdoptionListItem = (props: AdoptionListItemProps) => {
               ? theme.palette.text.primary
               : theme.palette.text.secondary,
           }}
-          onClick={() => navigateTo(navigate,"/admin/management/adoptions/1")}
+          onClick={() => navigateTo(navigate,`/admin/management/adoptions/${props.adoption.id}}`)}
         >
           Przejdź
         </Button>
